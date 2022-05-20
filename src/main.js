b9bc98be3984e6359a582091ef1cd72a1c287fef
hanjialiang <1192569357@qq.com>
2022-05-20 14:14:13 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
+import{createApp} from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+createApp(App).config.productionTip=false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app');
